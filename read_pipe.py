8c1dae202894ced28989a02bb2bf04bbe047d6df
K.-Michael Aye <aye@ess.ucla.edu>
2012-10-16 22:56:58 -0700

    struct unpacking working

--- read_pipe.py
+++ read_pipe.py
@@ -1,7 +1,13 @@
 import sys
 from collections import OrderedDict
 import struct
+from itertools import izip_longest
 
+def grouper(n, iterable, fillvalue=None):
+    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return izip_longest(fillvalue=fillvalue, *args)
+    
 def get_token(s):
     s = s.strip().strip("'").strip().strip("'").strip()
     return s
@@ -9,7 +15,7 @@ def get_token(s):
 d = OrderedDict()
 
 for i,line in enumerate(sys.stdin):
-    print len(line),repr(line)
+    # print len(line),repr(line)
     noOfChars = len(line)
     # short descriptors have 54 chars
     if noOfChars == 54:
@@ -27,3 +33,9 @@ for i,line in enumerate(sys.stdin):
 print("{0} lines of descriptor read.".format(i-1))
 print d
 print repr(data)
+
+n = len(d.keys())
+n *= 8 # for having 2 bytes = 16 bit for each item
+data = [data[i:i+n] for i in range(0, len(data), n)]
+for line in data:
+    print struct.unpack('dddd',line)
