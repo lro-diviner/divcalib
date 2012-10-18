237213e49e8504c5030730353287430c803d80d8
K.-Michael Aye <aye@ess.ucla.edu>
2012-10-17 19:45:41 -0700

    removin grouper helper. adding more elegant split_by_n. putting main code into main() function. adding struct.Struct object for unpacking.

--- read_pipe.py
+++ read_pipe.py
@@ -1,41 +1,50 @@
 import sys
 from collections import OrderedDict
 import struct
-from itertools import izip_longest
+import time
 
-def grouper(n, iterable, fillvalue=None):
-    "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return izip_longest(fillvalue=fillvalue, *args)
-    
+def split_by_n(seq, n):
+    while seq:
+        yield seq[:n]
+        seq = seq[n:]
+
 def get_token(s):
     s = s.strip().strip("'").strip().strip("'").strip()
     return s
 
-d = OrderedDict()
+def main():
+    """docstring for main"""
+    d = OrderedDict()
 
-for i,line in enumerate(sys.stdin):
-    # print len(line),repr(line)
-    noOfChars = len(line)
-    # short descriptors have 54 chars
-    if noOfChars == 54:
-        tmp = get_token(line)
-    # long descriptor names have 134 chars
-    elif noOfChars ==  134:
-        longdes = get_token(line)
-        d[tmp] = longdes
-    # the 'end' line has 15 but the very last 'line' is the data,
-    # so if we assign anything not 54 or 134 to data, at the end
-    # that's correct.
-    else:
-        data = line
-    
-print("{0} lines of descriptor read.".format(i-1))
-print d
-print repr(data)
+    while True:
+        line = sys.stdin.readline()
+        # print len(line),repr(line)
+        noOfChars = len(line)
+        # short descriptors have 54 chars
+        if noOfChars == 54:
+            tmp = get_token(line)
+        # long descriptor names have 134 chars
+        elif noOfChars ==  134:
+            longdes = get_token(line)
+            d[tmp] = longdes
+        elif noOfChars == 15:
+            break
 
-n = len(d.keys())
-n *= 8 # for having 2 bytes = 16 bit for each item
-data = [data[i:i+n] for i in range(0, len(data), n)]
-for line in data:
-    print struct.unpack('dddd',line)
+    # each colum is piped as a double, so 8 chars.
+    ncols = len(d.keys())
+    n = 8 * ncols
+
+    unpacker = struct.Struct(ncols*'d')
+
+    l = []
+    while True:
+        data = sys.stdin.read(n)
+        if not data: break
+        l.append(unpacker.unpack(data))
+        if (len(l) % 100000) == 0:
+            print('100 k lines read.')
+    print len(l), 'lines of data collected.'
+        
+        
+if __name__ == '__main__':
+    main()
