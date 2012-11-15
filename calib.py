92397c45ce41183a91ac72c21c1ae4e538f42048
K.-Michael Aye <aye@ess.ucla.edu>
2012-11-14 20:20:03 -0800

    adding CalibFlag and MiscFlag class that applies flag_data tuples to the Flag class

--- calib.py
+++ calib.py
@@ -1,52 +1,84 @@
+from collections import OrderedDict
+
 SV_LENGTH = 80
 
 SV_NUM_SKIP_SAMPLE = 16
 BBV_NUM_SKIP_SAMPLE = 16
 SOLV_NUM_SKIP_SAMPLE = 16
 
-#  Calibration quality mask
-calib_quality_values=dict(
-    #  Bit 0: Interpolation but one or more marker out of bounds
-	qf_interp_marker_oob					= 0x00000001,
-    #  Bit 1: Offsets and Gains from Nearest Marker used
-	qf_nearest_marker						= 0x00000002,
-    #  Bit 2: Same as bit 1, but nearest marker is out of bounds
-	qf_nearest_marker_oob					= 0x00000004,
-    #  Bit 3: Use constants for offsets and gains
-	qf_constants_only						= 0x00000008
-)
-
-#  Misc quality mask
-misc_quality_values=dict(
-    #  Bit 0: Reserved for future use
-	qf_misc_rfu0							= 0x00000001,
-	qf_misc_eclipse							= 0x00000002,
-	qf_misc_turn_on_transient				= 0x00000004,  
-	qf_misc_abnormal_instrument_state		= 0x00000008,
-	qf_misc_abnormal_instrument_temp_drift	= 0x00000010,
-	qf_misc_noise							= 0x00000020,
-	qf_misc_ch1_saturation					= 0x00000040,
-	qf_misc_moving							= 0x00000080 
-)
-
 class Flag(object):
     """Helper class to deal with control words or flags.
 
     Bit setting and checking methods are implemented.
     """
-    def __init__(self, value = 0,d = None):
+    def __init__(self, value = 0, dic = None):
         self.value = int(value)
-        if d:
-            self.d = d
-            for flag,val in d.iteritems():
-                    setattr(flag, self.check_bit(val))
+        if dic:
+            self.dic = dic
+            self.set_members()
+            
+    def set_members(self):
+        for flagname,val in self.dic.iteritems():
+            # add the flagname and it's status, but cut off initial
+            # qf_ from the name
+            setattr(self, flagname, self.check_bit(val))
     def set_bit(self, bit):
         self.value |= bit
+        self.set_members()
     def check_bit(self, bit):
         return self.value & bit != 0
     def clear_bit(self, bit):    
         self.value &= ~bit
+        self.set_members()
+    def __str__(self):
+        # find longest key
+        lwidth = 0
+        for key in self.dic.keys():
+            if len(key) > lwidth:
+                lwidth = len(key)
+        s = ''
+        for key in self.dic:
+            s += '{0} : {1}\n'.format(key.ljust(lwidth), 
+                                     str(getattr(self,key)).rjust(6))
+        return s
+    def __call__(self):
+        print(self.__str__())
 
+class CalibFlag(Flag):
+    flag_data=(
+        #  Bit 0: Interpolation but one or more marker out of bounds
+    	('interp_marker_oob', 0x00000001),
+        #  Bit 1: Offsets and Gains from Nearest Marker used
+    	('nearest_marker', 0x00000002),
+        #  Bit 2: Same as bit 1, but nearest marker is out of bounds
+    	('nearest_marker_oob', 0x00000004),
+        #  Bit 3: Use constants for offsets and gains
+    	('constants_only', 0x00000008),
+    )
+    flags = OrderedDict()
+    for t in flag_data:
+        flags[t[0]]=t[1]
+    def __init__(self,value=0):
+        super(CalibFlag,self).__init__(value,dic=self.flags)
+            
+class MiscFlag(Flag):
+    flag_data=(
+        #  Bit 0: Reserved for future use
+    	('rfu0', 0x00000001),
+    	('eclipse', 0x00000002),
+    	('turn_on_transient', 0x00000004),  
+    	('abnormal_instrument_state', 0x00000008),
+    	('abnormal_instrument_temp_drift', 0x00000010),
+    	('noise', 0x00000020),
+    	('ch1_saturation', 0x00000040),
+    	('moving', 0x00000080), 
+    )
+    # this convoluted way is necessary to keep the dictionary in order
+    flags = OrderedDict()
+    for t in flag_data:
+        flags[t[0]]=t[1]
+    def __init__(self,value=0):
+        super(MiscFlag,self).__init__(value,dic=self.flags)
 
 def get_offset_leftSV(chan, det):
     """docstring for get_offset_leftSV"""
