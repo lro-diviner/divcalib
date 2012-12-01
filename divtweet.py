4362bc7218bffc03e5d976201ec053b2dcf31478
K.-Michael Aye <aye@ess.ucla.edu>
2012-11-30 23:09:21 -0800

    making it functional as system cmd

--- divtweet.py
+++ divtweet.py
@@ -1,4 +1,6 @@
+#!/Library/Frameworks/Python.framework/Versions/Current/bin/python
 import tweepy
+import sys
 
 consumer_key='JnYy8IjOAthUtSEQmXYeg'
 consumer_secret='ORmxgR56WH2IhvJ8p3BbB3BGnulAhFVMyUM98zhUc'
@@ -8,5 +10,11 @@ access_secret='vxcXCQ6djUWBtXeWpWBXlUJD2CNUrkMnmXPaTYhzI'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
+
+text = " ".join(sys.argv[1:])
+
+if not text:
+    print("Usage: divtweet [username] this is my status update")
+api.update_status(text)
 
 
