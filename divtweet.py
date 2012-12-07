8f9daafb4b9bf699675e94f0a571c68ebde7c687
K.-Michael Aye <aye@ess.ucla.edu>
2012-12-07 12:12:27 -0800

    shifting code into main function

--- divtweet.py
+++ divtweet.py
@@ -1,4 +1,4 @@
-#!/Library/Frameworks/Python.framework/Versions/Current/bin/python
+#!/usr/bin/env python
 import tweepy
 import sys
 
@@ -11,10 +11,12 @@ auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
-text = " ".join(sys.argv[1:])
 
-if not text:
-    print("Usage: divtweet [username] this is my status update")
-api.update_status(text)
+if __name__ == "__main__":
+    text = " ".join(sys.argv[1:])
 
-
+    if not text:
+        print("Usage: divtweet [username] this is my status update")
+        sys.exit(1)
+    
+    api.update_status(text)
