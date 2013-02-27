641dfb5bd74883ca568828ce44c9b85e88a5c993
K.-Michael Aye <aye@ess.ucla.edu>
2013-02-26 19:30:56 -0800

    removing Views and CalibBlocks as this should be possible to do just with pandas groupings

--- calib.py
+++ calib.py
@@ -206,9 +206,17 @@ def get_blocks(df, blocktype, del_zero=True):
             pass
     return d
     
+def get_mean_time(df):
+    t1 = df.index[0]
+    t2 = df.index[-1]
+    t = t1 + (t2 - t1) // 2
+    return t
+
 class Calibrator(object):
-    """docstring for DivCalib"""
-    time_columns = ['year','month','date','hour','minute','second']
+    """currently set up to work with a 'wide' dataframe.
+    
+    Meaning, all detectors have their own column.
+    """
     def __init__(self, df):
         self.df = df
         # loading conversion table indexed in T*100 (for resolution)
@@ -231,52 +239,21 @@ class Calibrator(object):
                    
         #self.process_calib_blocks()
         
-    def old_setup(self, df):
-        # only read required columns from big array
-        self.dfsmall = df[self.time_columns + 
-                          ['c','det','counts','bb_1_temp','bb_2_temp',
-                           'el_cmd','az_cmd','qmi']]
-                           # 'clat','clon','scalt']]
-                           
-        # generate time index from time-related columns
-        index = fu.generate_date_index(self.dfsmall)
         
-        # change index from anonymous integers to ch,det,time and assign result
-        # to new dataframe
-        self.df = self.dfsmall.set_index(['c','det',index])
-        
-        # give the index columns a name
-        self.df.index.names = ['c','det','time']
-        
-        # having indexed by time i don't need the time data columns anymore
-        self.df = self.df.drop(self.time_columns, axis=1)
-        
-        self.all_times = pd.Index(df.index.get_level_values(2).unique())
-        
-        # define science datatypes and boolean views
-        define_sdtype(self.df)
-
-    def process_calib_blocks(self):
-        # create lists to save gains and offsets from the calib_blocks:
-        d = {}
-        # loop over calib blocks
-        for blockid, block in self.calib_blocks.iteritems():
-            try:
-                cblock = CalibBlock(block)
-            except ViewLengthError:
-                # if it's the last key the data is most likely at the limit of
-                # the dataset.
-                # FIXME later.
-                if blockid == sorted(self.calib_blocks.keys())[-1]:
-                    continue
-            d[blockid]=(cblock.bbview.mid_time, cblock.offset, cblock.gain)
-        self.calib_data = d
-        
-    #def interpolate_calib_data(self):
-    #    data = self.calib_data.values()
-    #    times = [i[0] for i in data]
-    #    offsets = [i[1] for i in data]
-    #    gains = [i[2] for i in data]
+    def get_offsets(self):
+        # get spaceviews here to kick out moving data
+        df = self.df[self.df.is_spaceview]
+        # group by the calibration block labels
+        grouped = df.groupby(df.calib_block_labels)
+        # get the mean times for each calib block
+        times = grouped.a3_11.apply(get_mean_time)
+        ###
+        # change here for method of means!!
+        ###
+        offsets = grouped.mean()
+        # set the times as index for this dataframe of offsets
+        offsets.index = times
+        return offsets
         
     def interpolate_bb_temps(self):
         # just a shortcutting reference
@@ -358,183 +335,59 @@ class Calibrator(object):
         
 
 
-class CalibBlock(object):
-    """Deal with a block of calibration data as defined by define_sdtypes.
-    
-    Because it's just defined by pointing commands, this data block contains 
-    moving data that needs to be ignored. The advantage of not throwing moving
-    data away earlier, that successive calibration activities can be recognized
-    as such.
         
-    >>> cb = CalibBlock(df)
-    >>> cb.start_time
-    <timestamp>
-    >>> cb.end_time
-    <timestamp>
-    >>> cb.mean_time
-    <timestamp>
-    >>> cb.offset
-    <value>
-    """
-    def __init__(self, df):
-        self.df = df
-        
-        # as the incoming df should only be exactly one calib block I can just 
-        # pick the first item in the calib_block_labels
-        self.id = df.calib_block_labels[0]
-        
-        # set times for this calib block
-        self.get_times()
-        
-        # Find and process spaceviews for object
-        self.process_spaceviews()
-                
-        # self.process_bbview()
-        
-        # self.gain = self.calc_gain()
-
-        # # levels[2] to pick out the timestamp from hierarchical index
-        # self.alltimes = pd.Index(self.df.index.get_level_values(2).unique())
-        # self.start_time_moving = self.alltimes[0]
-        # self.end_time_moving = self.alltimes[-1]
-        
-    def plot(self, **kwargs):
-        pu.plot_calib_block(self.df,'calib',self.id, **kwargs)
-        
-    def get_times(self):
-        self.start_time = self.df.index[0]
-        self.end_time = self.df.index[-1]
-        self.mid_time = self.start_time + (self.end_time - self.start_time)//2
-        
-    def process_bbview(self):
-        """Process the bbview inside this CalibBlock.
-        
-        Defines:
-        --------
-        * self.bbv_label
-        ** the ID of this BB view within this dataset
-        * self.bbview
-        ** the BBView object for this bb-view
-        
-        """
-        bbview_group = get_blocks(self.df, 'bb')
-        if len(bbview_group) == 0:
-            raise NoOfViewsError('bb', '>0', 0, 'process_bbview')
-        self.bbv_label = bbview_group.keys()
-        self.bbview = BBView(bbview_group[self.bbv_label[0]])
-        
-    def process_spaceviews(self):
-        """Process the spaceviews inside this CalibBlock.
-        
-        Defining the spaceviews by creating:
-        --------
-        * self.spaceviews
-        * self.sv_labels
-        * self.left_sv
-        * self.right_sv
-        """
-        # get spaceviews, has keys and df in blocks
-        spaceviews = get_blocks(self.df, 'sv')
-                
-        if spaceviews:
-            # get the 2 item list of spaceview labels and sort them
-            self.sv_labels = sorted(spaceviews.keys())
-            
-            # store spaceviews in a dictionary            
-            self.spaceviews = {}
-            for label in self.sv_labels:
-                self.spaceviews[label] = SpaceView(spaceviews[label])
-            self.get_offset()
-        
-        
-    def get_offset(self, method='all',det='a3_11'):
-        """calculate offset.
-        
-        TODO: Deal with solar target containers differently?
-        
-        Parameters
-        ----------
-        method: string
-            Values: ['all','first','last'] to determine which sides of the 
-            spaceviews are being used for the offset calculation.
-            
-        Returns:
-        --------
-        offset: pandas.Series
-            data column copied attached to self
-        """
-        # get only the spaceviews now (excludes moving data, labels do not)
-        subdf = self.df[self.df.is_spaceview]
-        
-        if method == 'all':
-            offset = subdf[det].mean()
-        elif method == 'first':
-            offset = subdf[subdf.sv_block_label==self.sv_labels[0]][det].mean()
-        elif method == 'last':
-            offset = subdf[subdf.sv_block_label==self.sv_labels[-1]][det].mean()
-        else:
-            raise UnknownMethodError(method, 'CalibBlock.get_offset')
-        self.offset = offset
-        return offset
-        
-    def calc_gain(self):
-        """Calc gain.
-        
-        This is how JPL did it:
-        numerator = -1 * thermal_marker_node.calcRBB(chan,det)
-        
-        Then a first step for the denominator:
-        denominator = (thermal_marker_node.calc_offset_leftSV(chan, det) + 
-                       thermal_marker_node.calc_offset_rightSV(chan,det)) / 2.0
-        
-        Basically, that means: denominator = mean(loffset, roffset)
-        
-        For the second step of the denominator, they used calc_CBB, which is just 
-        the mean of counts in BB view:
-        denominator -= thermal_marker_node.calc_CBB(chan, det)
-        return numerator/denominator.
-        """
-        numerator = -1 * self.bbview.rbb_average
-        denominator = self.offset - self.bbview.average
-        return numerator / denominator
+    #def plot(self, **kwargs):
+    #    pu.plot_calib_block(self.df,'calib',self.id, **kwargs)
+    #    
+    #def get_times(self):
+    #    self.start_time = self.df.index[0]
+    #    self.end_time = self.df.index[-1]
+    #    self.mid_time = self.start_time + (self.end_time - self.start_time)//2
+    #    
+    #def process_bbview(self):
+    #    """Process the bbview inside this CalibBlock.
+    #    
+    #    Defines:
+    #    --------
+    #    * self.bbv_label
+    #    ** the ID of this BB view within this dataset
+    #    * self.bbview
+    #    ** the BBView object for this bb-view
+    #    
+    #    """
+    #    bbview_group = get_blocks(self.df, 'bb')
+    #    if len(bbview_group) == 0:
+    #        raise NoOfViewsError('bb', '>0', 0, 'process_bbview')
+    #    self.bbv_label = bbview_group.keys()
+    #    self.bbview = BBView(bbview_group[self.bbv_label[0]])
+    #    
+    #            
+    #def calc_gain(self):
+    #    """Calc gain.
+    #    
+    #    This is how JPL did it:
+    #    numerator = -1 * thermal_marker_node.calcRBB(chan,det)
+    #    
+    #    Then a first step for the denominator:
+    #    denominator = (thermal_marker_node.calc_offset_leftSV(chan, det) + 
+    #                   thermal_marker_node.calc_offset_rightSV(chan,det)) / 2.0
+    #    
+    #    Basically, that means: denominator = mean(loffset, roffset)
+    #    
+    #    For the second step of the denominator, they used calc_CBB, which is just 
+    #    the mean of counts in BB view:
+    #    denominator -= thermal_marker_node.calc_CBB(chan, det)
+    #    return numerator/denominator.
+    #    """
+    #    numerator = -1 * self.bbview.rbb_average
+    #    denominator = self.offset - self.bbview.average
+    #    return numerator / denominator
                       
 
-class View(object):
-    """methods to deal with spaceviews.
     
-    Each View object should offer a simple average and also a more intricate
-    way to determine the count values of itself. (Like cutting of samples on the left
-    or right side.)
-    """
-    def __init__(self, df):
-        self.df = df
-        self.start_time = self.df.index[0]
-        self.end_time   = self.df.index[-1]
-        self.mid_time =  self.start_time + (self.end_time-self.start_time)//2
-        # self.average = self.counts.groupby(level=['c','det']).mean()
-        
-    def get_counts_mean(offset_left=0,offset_right=0):
-        "Placeholder for getting counts in different ways."
-        pass
-
-    def __len__(self):
-        "provide own answer to length for the safety checks in CalibBlock()"
-        return len(self.counts)
-    
-        
-class SpaceView(View):
-    """docstring for SpaceView"""
-    def __init__(self, df):
-        super(SpaceView, self).__init__(df)
-    
-class BBView(View):
-    """docstring for BBView"""
-    def __init__(self, df):
-        super(BBView, self).__init__(df)
-        self.RBB = df.RBB
         # meaningfull is only the grouping on level=['c'] but we need the c,det indexing
         # anyway later so I might as well have the broadcasting here.
-        self.rbb_average = self.RBB.groupby(level=['c','det']).mean()
+        #self.rbb_average = self.RBB.groupby(level=['c','det']).mean()
         
 
 #def thermal_alternative():
