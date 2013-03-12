5e658492c919b159102508f9db23834eda536ec5
K.-Michael Aye <aye@ess.ucla.edu>
2013-03-12 15:56:52 -0700

    adding filter for futurewarning

--- compare_calibs.py
+++ compare_calibs.py
@@ -3,16 +3,19 @@ import pandas as pd
 import file_utils as fu
 import calib as c
 import matplotlib.pyplot as plt
+import warnings
 
 print pd.__version__
 
+warnings.filterwarnings('ignore',category=FutureWarning)
+    
 #
 # first get divdata file
 #
 
-divrad_fname = '/Users/maye/data/diviner/rdr_data/2011041600_c3d11.divdata'
+divrad_fname = '/Users/maye/data/diviner/rdr_data/20110416_00-01_c3d11.divdata'
 
-columns = ['year','month','date','hour','minute','second','radiance']
+columns = ['year','month','date','hour','minute','second','qmi','radiance']
 
 # use pandas parser to read in text file
 divdata = pd.io.parsers.read_table(divrad_fname, sep='\s+',names=columns)
@@ -23,25 +26,21 @@ divdata = fu.index_by_time(divdata)
 # sort it
 divdata = divdata.sort_index()
 
+# drop qmi
+divdata = divdata.drop('qmi',axis=1)
+
 #
 # now get div247 file and calibrate
 #
 # get data pump
 pump = fu.Div247DataPump("20110416")
 
-fnames = list(pump.gen_fnames())
-
-df1 = pump.process_one_file(fnames[0])
-df2 = pump.process_one_file(fnames[1])
-df = pd.concat([df1,df2])
-# df = df1
-df = pump.clean_final_df(df)
-
 # get first hour for that day
-# df = pump.get_n_hours(2)
+df = pump.get_n_hours(2)
 
 #calibrate
 calib = c.Calibrator(df)
+calib.calibrate()
 
 myrad = pd.DataFrame(calib.abs_radiance.a6_11)
 
@@ -55,4 +54,6 @@ compare.rel_error.plot()
 # plt.legend(loc='best')
 plt.savefig(divrad_fname+'.png',dpi=100)
 # plt.show()
+print
+
 
