d32f21627877ab3d6a06ff3adff44b7425989c58
K.-Michael Aye <aye@ess.ucla.edu>
2013-04-22 20:11:01 -0700

    cleaning up plot_calib_block

--- plot_utils.py
+++ plot_utils.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from matplotlib.pylab import gcf,title, subplots
 import sys
+import pandas as pd
 
 def save_to_www(fname, **kwargs):
     gcf().savefig("/u/paige/maye/WWW/calib/"+fname,**kwargs)
@@ -20,15 +21,15 @@ def plot_calib_block(df,label,id,det='a6_11'):
     if not label.endswith('_block_labels'):
         label = label + '_block_labels'
     dfnow = df[df[label]==id]
-    l = ['is_moving','is_spaceview','is_bbview','is_stview']
-    lnew = []
-    for item in l:
-        dfnow2 = dfnow[dfnow[item]][det]
-        if len(dfnow2) > 0:
-            nitem = item.replace('is_','')
-            dfnow[nitem] = dfnow2
-            lnew.append(nitem)
-    dfnow[lnew].plot(style='.',linewidth=2)
+    df_to_plot = pd.DataFrame(index=dfnow.index)
+    boolean_selectors = ['is_moving','is_spaceview','is_bbview','is_stview']
+    for sel in boolean_selectors:
+        # get the timeseries for chosen detector where selector is true
+        timeseries = dfnow[dfnow[sel]][det]
+        if len(timeseries) > 0:
+            # add to dataframe, cut off 'is_' from name (nicer for plot)
+            df_to_plot[sel[3:]] = timeseries
+    df_to_plot.plot(style='.')
     title(det)
 
 def plot_all_channels(df, det_list, **kwargs):
