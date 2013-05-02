a0e1101dd3641b3d820e524c2fc3887f4057314f
K.-Michael Aye <aye@ess.ucla.edu>
2013-05-02 15:50:59 -0700

    changing path for planet_spk

--- kernel_loader.py
+++ kernel_loader.py
@@ -12,7 +12,7 @@ spice.furnsh("ick/lrodv_2010090_2010121_v01.bc")
 spice.furnsh("lsk/naif0010.tls")
 spice.furnsh('sclk/lro_clkcor_2010096_v00.tsc')
 spice.furnsh("spk/fdf29_2010100_2010101_n01.bsp")
-spice.furnsh('./spk/de421.bsp')
+spice.furnsh('./planet_spk/de421.bsp')
 
 utc = dt.strptime('2010100 10','%Y%j %H').isoformat()
 et = spice.utc2et(utc)
