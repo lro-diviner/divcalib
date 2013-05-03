e557ac6d33549106f1f0beb58f65fe57f1d73329
K.-Michael Aye <aye@ess.ucla.edu>
2013-05-02 19:09:20 -0700

    adding get_times_from_ck

--- kerneltools.py
+++ kerneltools.py
@@ -1,12 +1,12 @@
 # coding: utf-8
-import os.path as path
+import os
 import spice
 from datetime import datetime as dt
 from file_utils import kernelpath
 
 def get_version_from_fname(fname):
     # cut off extension
-    pre_ext = path.splitext(fname)[0]
+    pre_ext = os.path.splitext(fname)[0]
     # split by _ and get last element (the version number string, if available)
     if not '_' in pre_ext:
         return None
@@ -15,3 +15,17 @@ def get_version_from_fname(fname):
         return None
     return int(last_token[1:])
     
+def get_times_from_ck(fname):
+    "Parse times from a ck filename."
+    # ex: 'moc42_2009099_2009100_v01.bc'
+    # i checked: all ck files start with moc42
+    tokens = os.path.splitext(fname)[0].split('_')
+    startstr, endstr = tokens[1], tokens[2]
+    formatstr = '%Y%j'
+    starttime = dt.strptime(startstr, formatstr)
+    endtime = dt.strptime(endstr, formatstr)
+    return (starttime, endtime)
+    
+def load_kernels_for_timestamp(timestamp):
+    number_of_kernels_loaded = 0
+    return number_of_kernels_loaded
--- tests/test_kerneltools.py
+++ tests/test_kerneltools.py
@@ -26,18 +26,21 @@ def test_example_loading():
         assert round(i,3) == round(j,3)
     os.chdir(currentdir)
         
-def test_load_for_time():
-    pass
-    
 def test_get_version_from_fname():
     fname = 'moc42_2009099_2009100_v01.bc'
     version = 1
     assert version == kerneltools.get_version_from_fname(fname)
     
-def test_get_times_from_ck_fname():
+def test_get_times_from_ck():
     fname = 'moc42_2009099_2009100_v01.bc'
     start_time = dt.strptime('2009099','%Y%j')
     end_time = dt.strptime('2009100','%Y%j')
+    result = kerneltools.get_times_from_ck(fname)
+    assert start_time == result[0]
+    assert end_time == result[1]
     
-if __name__ == '__main__':
-    test_loading()
+def test_load_kernels_for_timestamp():
+    time = dt(2010,10,10)
+    nr_kernels_to_load = 8
+    assert kerneltools.load_kernels_for_timestamp(time) == nr_kernels_to_load
+    
