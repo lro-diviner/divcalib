eefae66c9ecf065725acb94589ea96ac221d0a8f
K.-Michael Aye <aye@ess.ucla.edu>
2013-05-07 20:51:49 -0700

    adding half-test for Div247DataPump

--- tests/test_file_utils.py
+++ tests/test_file_utils.py
@@ -1,2 +1,11 @@
 from diviner import file_utils as fu
 
+class TestDiv247DataPump():
+    """docstring for test_Div247DataPump"""
+    @classmethod
+    def setup_class(cls):
+        pump = fu.Div247DataPump('20110416')
+        
+    def test_find_fnames(self):
+        pass
+
