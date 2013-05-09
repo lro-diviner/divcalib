a24d8fbdfadfe56f6a10b5b5da43e985569fd2a5
K.-Michael Aye <aye@ess.ucla.edu>
2013-05-08 17:30:53 -0700

    pep8ified

--- kerneltools.py
+++ kerneltools.py
@@ -3,7 +3,12 @@ import os
 import spice
 from datetime import datetime as dt
 from file_utils import kernelpath
+import glob
 
+# from ConfigParser import ConfigParser
+#
+# config = ConfigParser()
+# config.read('level1b.config')
 
 def get_version_from_fname(fname):
     # cut off extension
@@ -15,9 +20,9 @@ def get_version_from_fname(fname):
     if not last_token.startswith('v'):
         return None
     return last_token, int(last_token[1:])
-    
 
-def get_times_from_ck(fname): 
+
+def get_times_from_ck(fname):
     "Parse times from a ck filename. Returns python datetime objects."
     # ex: 'moc42_2009099_2009100_v01.bc'
     # i checked: all ck files start with moc42
@@ -27,27 +32,29 @@ def get_times_from_ck(fname):
     starttime = dt.strptime(startstr, formatstr)
     endtime = dt.strptime(endstr, formatstr)
     return (starttime, endtime)
-    
+
 
 def load_kernels_for_timestamp(timestamp):
     number_of_kernels_loaded = 0
     return number_of_kernels_loaded
-  
-def get_available_ckernels()  :
+
+
+def get_available_ckernels():
     root = os.path.join(kernelpath, 'ck')
     return os.listdir(root)
+
 
 class CKFileName(object):
     """Class to create and handle CK file names."""
     extension = 'bc'
-    prefix = 'moc42' # have not seen any other one
-    root = os.path.join(kernelpath,'ck')
-    
+    prefix = 'moc42'  # have not seen any other one
+    root = os.path.join(kernelpath, 'ck')
+
     @classmethod
     def from_fname(cls, fname):
         "Including this for better readability. same as CKFileName(fname=fname)"
         return cls(fname=fname)
-        
+
     @classmethod
     def from_timestr(cls, timestr):
         return cls(timestr=timestr)
@@ -63,7 +70,6 @@ class CKFileName(object):
         elif timestr is not None:
             self.timestr = timestr
             time = dt.strptime(timestr, '%Y%m%d%H')
-            
+
         else:
-            raise Exception('CKFileName','You have to provide either timestr or fname.')
-            
+            raise Exception('CKFileName', 'You have to provide either timestr or fname.')
