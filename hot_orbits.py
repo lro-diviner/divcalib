26d50b007bc87b83f2a6a48fa165453667d03e86
K.-Michael Aye <aye@ess.ucla.edu>
2013-06-20 18:46:07 -0700

    updated...

--- hot_orbits.py
+++ hot_orbits.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 from __future__ import division, print_function
-from matplotlib.pylab import *
+from matplotlib.pyplot import show, rcParams, subplots
 from diviner import file_utils as fu
 from diviner import calib
 import pandas as pd
 from diviner import file_utils as fu
+from datetime import timedelta
 
 rcParams['figure.figsize'] = (14, 10)
 
@@ -14,36 +15,59 @@ class HotOrbits(object):
     """docstring for HotOrbits"""
     savepath = '/Users/maye/Dropbox/DDocuments/DIVINER/calib/hot_orbits/jpl_meeting/'
 
-    def __init__(self, ch2study, df, title_prefix=''):
+    def __init__(self, ch2study, df, title_prefix='', rad_corr=True, 
+                 do_negative_corr=False):
         super(HotOrbits, self).__init__()
         self.ch2study = ch2study
         self.df = df
         self.title_prefix = title_prefix
+        self.rad_corr = rad_corr
+        self.do_negative_corr = do_negative_corr
         
         # my calibration
-        c = calib.Calibrator(df, single_rbb=False)
+        c = calib.Calibrator(df, do_rad_corr=rad_corr, 
+                                 do_negative_corr=do_negative_corr)
         c.calibrate()
         self.c = c
         
+    def set_ch2study(self, ch):
+        ch2study = ch
+        self.ch2study = ch
         # get only Tb
-        Tb_all = calib.get_data_columns(c.Tb)
+        Tb_all = calib.get_data_columns(self.c.Tb)
         Tb = Tb_all.filter(regex=ch2study + '_')
         
         # make all detectors integer numbers
         Tb = Tb.rename(columns=lambda x: int(x[3:]))
-        self.Tb = Tb
-        self.chmedian = Tb.apply(lambda x: x - x.median(), axis=1)
-        self.chmean = Tb.apply(lambda x: x - x.mean(), axis=1)
+
+        # cut off first 30 minutes for boundary effects
+        t0 = Tb.index[0]
+        Tb = Tb.ix[(t0 + timedelta(minutes=30)):]
+               
+        self.Tb = Tb.resample('10s')
+        self.chmedian = self.Tb.apply(lambda x: x - x.median(), axis=1)
+        self.chmean = self.Tb.apply(lambda x: x - x.mean(), axis=1)
         
     def plot_detectors(self, det_list=[1,11,21]):
-        # figure()
-        self.Tb[det_list].plot()
-        legend()
-        ylabel('Tb [K]')
-        xlabel('Time [UTC]')
-        titlestr = self.ch2study + ' Tb Det 01 11 21'
-        title(' '.join([self.title_prefix, titlestr]))
-        savefig(self.savepath + '_'.join(titlestr.split()) + '.png')
+        fig, ax = subplots()
+
+        self.Tb[det_list].plot(ax=ax)
+        ax.legend()
+        ax.set_ylabel('Tb [K]')
+        ax.set_xlabel('Time [UTC]')
+        # ax.set_ylim(ymin=-100,ymax=400)
+        corr_string = self.get_corr_string()
+        titlestr = '{0} Tb Det 01 11 21 _ {1}'.format(self.ch2study, corr_string)
+        ax.set_title(' '.join([self.title_prefix, titlestr]))
+        savestring = self.savepath + '_'.join(titlestr.split()) + '.png' 
+        print(savestring)
+        fig.savefig(savestring, dpi=200)
+
+    def get_save_string(self):
+        corr_string = self.get_corr_string()
+        titlestr = '{0} Tb Det 01 11 21 _ {1}'.format(self.ch2study, corr_string)
+        savestring = self.savepath + '_'.join(titlestr.split()) + '.png'
+        return savestring
 
     def plot_raw(self, ):
         df = self.df
@@ -69,25 +93,45 @@ class HotOrbits(object):
         xlabel('Time [UTC')
         savefig(self.savepath + '_'.join(titlestr.split()) + '.png')
     
-    def plot_lips(self, kind='mean'):
-        Tb = self.Tb.resample('10s')
+        
+    def get_corr_string(self):
+        if self.rad_corr:
+            if self.do_negative_corr:
+                corr_string = 'INV_corrected'
+            else:
+                corr_string = 'corrected'
+        else:
+            corr_string = 'uncorrected'
+        return corr_string
+        
+    def plot_lips(self, kind='mean', separate=True):
+
+        fig, ax = subplots()
+        
         if kind == 'median':
             df = self.chmedian
         elif kind == 'mean':
             df = self.chmean
-        
-        figure()
-        df.plot()
-        # for i in range(1, 12):
-        #     df[i].plot(style='r')
-        # for i in range(12, 22):
-        #     df[i].plot(style='g')
-        # df[21].plot(style='c')
-        legend(ncol=5)
-        titlestr = self.ch2study + ' Tb Deviations from {0} detector sides sorted'.format(kind)
-        title(' '.join([self.title_prefix, titlestr]))
-        savefig(self.savepath + '_'.join(titlestr.split()) + '.png')
-        
+
+        if separate:
+            for i in range(1, 12):
+                df[i].plot(style='r', ax=ax)
+            for i in range(12, 22):
+                df[i].plot(style='g', ax=ax)
+            df[21].plot(style='k', ax=ax)
+        else:
+            df.plot(ax=ax)
+        ax.legend(ncol=5,loc='lower right')
+        # ax.set_ylim(ymin=-80,ymax=60)
+
+        corr_string = self.get_corr_string()
+        titlestr = self.ch2study + \
+            ' Tb Deviations from {0} detector sides sorted_{1}'.format(kind, corr_string)
+        ax.set_title(' '.join([self.title_prefix, titlestr]))
+        savestring = self.savepath + '_'.join(titlestr.split()) + '.png'
+        print(savestring)
+        fig.savefig(savestring, dpi=200)
+            
     def imshow(self):
         figure(figsize=(21,3))
         imshow(self.Tb.values.T, interpolation='none', aspect='auto')  
@@ -129,23 +173,35 @@ class HotOrbitsDivData(HotOrbits):
 ####
 
 # Channel to study:
-ch2study = 'b3'
+ch2study = 'a5'
 
 # get L1A data
 timestr = '20110402'
 offset = 4
-# pump = fu.Div247DataPump(timestr)
-# df = pump.get_n_hours_from_t(4, offset)
-# 
-# hotorbits = HotOrbits(ch2study, df, timestr + str(offset))
+pump = fu.Div247DataPump(timestr)
+df = pump.get_n_hours_from_t(6, offset)
 
-# hotorbits.plot_detectors()
-
+hotorbits_corr = HotOrbits(ch2study, df, timestr + str(offset), rad_corr=True)
+hotorbits_uncorr = HotOrbits(ch2study, df, timestr + str(offset), rad_corr=False)
+hotorbits_negcorr = HotOrbits(ch2study, df, timestr + str(offset), rad_corr=True,
+                              do_negative_corr=True)
+                        
+detectors = ['a3','a4','a5','a6','b1','b2','b3']
+# detectors = ['b3']
+for ch in detectors:
+    print("Doing", ch)
+    hotorbits_corr.set_ch2study(ch)
+    hotorbits_uncorr.set_ch2study(ch)
+    hotorbits_negcorr.set_ch2study(ch)
+    hotorbits_corr.plot_detectors()
+    hotorbits_uncorr.plot_detectors()
+    hotorbits_negcorr.plot_detectors()
 # hotorbits.plot_raw()
-
 # hotorbits.imshow()
-
-# hotorbits.plot_lips()
+    hotorbits_corr.plot_lips(kind='mean', separate=True)
+    hotorbits_uncorr.plot_lips(kind='mean', separate=True)
+    hotorbits_negcorr.plot_lips(kind='mean', separate=True)
+    
 # hotorbits.plot_max_deviation_det()
 # hotorbits.plot_kde()
 
@@ -158,19 +214,19 @@ offset = 4
 # Tb.std()
 
 
-divdata = pd.io.parsers.read_csv('/Users/maye/data/diviner/hot_orbit_ch9.txt', sep=r'\s*', names=['year', 'month', 'date', 'hour', 'minute', 'second', 'det', 'tb', 'qca'])
+# divdata = pd.io.parsers.read_csv('/Users/maye/data/diviner/hot_orbit_ch9.txt', sep=r'\s*', names=['year', 'month', 'date', 'hour', 'minute', 'second', 'det', 'tb', 'qca'])
+# 
+# df = fu.index_by_time(divdata)
+# print("\Index for divdata:", df.index)
+# print("\nqca: ",df.qca.unique())
+# 
+# df = df.reset_index()
+# df = df.pivot(index='index', columns='det', values='tb')
+# df.columns = range(1,22)
+# divhotorbits = HotOrbitsDivData(ch2study, df, timestr + str(offset) + '_divdata')
+# divhotorbits.plot_detectors()
+# divhotorbits.imshow()
+# divhotorbits.plot_max_deviation_det()
+# divhotorbits.plot_kde()
 
-df = fu.index_by_time(divdata)
-print("\Index for divdata:", df.index)
-print("\nqca: ",df.qca.unique())
-
-df = df.reset_index()
-df = df.pivot(index='index', columns='det', values='tb')
-df.columns = range(1,22)
-divhotorbits = HotOrbitsDivData(ch2study, df, timestr + str(offset) + '_divdata')
-divhotorbits.plot_detectors()
-divhotorbits.imshow()
-divhotorbits.plot_max_deviation_det()
-divhotorbits.plot_kde()
-
-show()
+#show()
