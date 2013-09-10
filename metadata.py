8289e013e6e87e7ccccf964c903eba18488921a4
K.-Michael Aye <aye@ess.ucla.edu>
2013-09-09 20:58:16 -0700

    adding divmetadata production

--- metadata.py
+++ metadata.py
@@ -5,13 +5,13 @@ import sys
 import pandas as pd
 import os
 from diviner import divtweet
+from subprocess import call
 
 savedir = os.path.join(fu.outpath,'metadata')
 if not os.path.exists(savedir):
     os.makedirs(savedir)
 
 def collect_data(pump):
-
     frames = []
     metadatacols = list(set(pump.keys) - set(fu.L1AHeader.datacols))
     for fname in pump.fnames:
@@ -38,14 +38,7 @@ def produce_store_file(timestr):
     store.close()
     print("Done.")
     
-
-if __name__ == '__main__':
-# try:
-#     timestr = sys.argv[1]
-# except IndexError:
-#     print("Usage: {0} timestr".format(sys.argv[0]))
-#     sys.exit()
-
+def produce_store_file_main():
     try:
         year = sys.argv[1]
     except IndexError:
@@ -57,4 +50,26 @@ if __name__ == '__main__':
         divtweet.tweet_machine("Producing metadata for {0}".format(timestr))
         print("Producing", timestr)
         produce_store_file(timestr)
+    
+def divmetadata():
+    if len(sys.argv) < 3:
+        print("Usage: {0} month_start month_end [YYYYMM]".format(sys.argv[0]))
+        sys.exit()
+    months = pd.period_range(sys.argv[1],sys.argv[2], freq='M')
+    cmd_middle = ("clat=-90,90 c=3,3 det=11,11 | pextract extract=year,month,date,hour,"
+      "minute,second,jdate,orbit,sundst,sunlat,sunlon,sclk,sclat,sclon,scrad,scalt,"
+      "el_cmd,az_cmd,af,vert_lat,vert_lon,vlookx,vlooky,vlookz,clat,clon,cemis,"
+      "csunzen,csunazi,cloctime,qca,qge,qmi,qual | pprint titles=0 > ")
+    for month in months:
+        print("Producing metadata for month {0}".format(month))
+        ts = month.to_timestamp()
+        cmd_base = 'divdata daterange={0}'.format(ts.strftime('%Y%m '))
+        outfname = os.path.join(savedir, 
+                                '{0}_divmetadata.csv'.format(ts.strftime('%Y%m')))
+        cmd = cmd_base + cmd_middle + outfname
+        call(cmd, shell=True)
+        divtweet.tweet_machine("Metadata for month {0} finished.".format(month))
+
+if __name__ == '__main__':
+    divmetadata()
     
