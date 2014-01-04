fa6704a1d9b507d9aa39395b65bede6e958dca19
K.-Michael Aye <aye@ess.ucla.edu>
2014-01-03 21:35:04 -0800

    adding some first tests for CalBlock

--- tests/test_calib.py
+++ tests/test_calib.py
@@ -2,21 +2,16 @@ from __future__ import division, print_function
 import pytest
 
 @pytest.fixture
-def df():
-    """Return a dataframe for testing."""
-    from diviner import file_utils as fu
-    df = fu.L1ADataFile.from_timestr('2012012414').open()
-    return df
-    
-    
-def test_calblock(df):
+def cb(df):
+    """Return a CalBlock object for testing."""
     from diviner import calib
-    import sys
-    print(df.info())
+    from diviner import divconstants as config
     calblocks = calib.get_calib_blocks(df, 'calib')
-    for g,v in calblocks.iteritems():
-        print(g)
-        sys.stdout.flush()
-        cb = calib.CalBlock(v)
-        assert cb.mean_time
+    return calib.CalBlock(calblocks[1], config.SV_NUM_SKIP_SAMPLE)
     
+class TestCalBlock:
+    def test_sv_grouped(self, cb):
+        assert len(cb.sv_grouped) == 2
+    def test_sv_labels(self, cb):
+        assert cb.sv_labels[0] == 1
+        assert cb.sv_labels[1] == 2
