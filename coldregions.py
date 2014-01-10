00cd58c9e51220f4313c7d715b0de265fd9e405e
K.-Michael Aye <aye@ess.ucla.edu>
2014-01-10 13:07:52 -0800

    fixing things for LPSC14 abstract run

--- coldregions.py
+++ coldregions.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function, division
 import pandas as pd
-from diviner import file_utils as fu, data_prep as dp, calib, plot_utils as pu
-import numpy as np
+import file_utils as fu
+import calib
 from multiprocessing import Pool
 import os
 import sys
+import logging
+
+root = '/raid1/maye/coldregions'
+logging.basicConfig(filename='log_coldregions.log', level=logging.INFO)
 
 def get_l1a_timestring(val):
     dt = val.to_pydatetime()
@@ -19,24 +23,15 @@ def get_column_from_timestr(t, col):
     c.calibrate()
     return getattr(c, col)
 
-def process_one_timestring(t):
-    savename = root + 'tstring_'+t+'.h5'
+def process_one_timestring(args):
+    t, region = args
+    savename = os.path.join(root,'tstring_'+t+'.h5')
     # if os.path.exists(savename):
     #     print "Skipping",t,'. Already done.'
     #     return
     print(t)
     region_now = region[region.filetimestr == t]
-    try:
-        newrad = get_col_from_timestr(t, 'norm_radiance')
-    except TypeError:
-        print("Got TypeError for", t)
-        return
-    except calib.MeanTimeCalcError:
-        print("Caught MeanTimeCalcError. Skipping", t)
-        return
-    except:
-        print("Caught unknown error. Skipping", t)
-        return
+    newrad = get_column_from_timestr(t, 'norm_radiance')
     dets = region_now.det.unique()
     container = []
     for det in dets:
@@ -46,33 +41,34 @@ def process_one_timestring(t):
         container.append(joined.rename(
                     columns=lambda x: 'newrad' if x.startswith('b3_') else x))
     newregion = pd.concat(container)
-    newregion.to_hdf(root + 'tstring_'+t+'.h5','df')
+    newregion.to_hdf(savename,'df')
 
 
 if __name__ == '__main__':
-    root = "/raid1/maye/coldregions/"
 
-    sps = []
-    for region in '1 3 5'.split():
-        fname = root + 'region_sp' + region + '_9.txt'
-        sps.append(pd.read_csv(fname, sep='\s*'))
+#    sps = []
+#    for region in '1 3 5'.split():
+#        fname = root + 'region_sp' + region + '_9.txt'
+#        sps.append(pd.read_csv(fname, sep='\s*'))
+#
+#    for df in sps:
+#        df.second = np.round(df.second * 1000) / 1000
+#        for col in ['year', 'month','date','hour','minute','det']:
+#            df[col] = df[col].astype('int')
+#        for col in ['year', 'month','date','hour','minute','second']:
+#            df[col] = df[col].astype('string')
+#        df.index = pd.to_datetime(df.year + '-' + df.month + '-' + df.date + ' ' +
+#                   df.hour + ':' + df.minute + ':' + df.second, utc=True)
+#        df['filetimestr'] = df.index.map(get_l1a_timestring)
 
-    for df in sps:
-        df.second = np.round(df.second * 1000) / 1000
-        for col in ['year', 'month','date','hour','minute','det']:
-            df[col] = df[col].astype('int')
-        for col in ['year', 'month','date','hour','minute','second']:
-            df[col] = df[col].astype('string')
-        df.index = pd.to_datetime(df.year + '-' + df.month + '-' + df.date + ' ' +
-                   df.hour + ':' + df.minute + ':' + df.second, utc=True)
-        df['filetimestr'] = df.index.map(get_l1a_timestring)
-
-    # pick region via argv, like so: python coldregions.py 0 [or 1, or 2]
-    region = sps[int(sys.argv[1])]
+    # pick region via argv, like so: python coldregions.py 1 [or 3, or 5]
+    region = pd.read_hdf(root+'/sps.h5','region'+sys.argv[1])
     
     timestrings = region.filetimestr.unique()
+    no = len(timestrings)
+    combined = zip(timestrings, no*[region])
     p = Pool(12)
-    p.map(process_one_timestring, timestrings)
+    p.map(process_one_timestring, combined)
 
     # for sp, region in zip(sps,'1 3 5'.split()):
     #     newsp = process_one_region(sp)
