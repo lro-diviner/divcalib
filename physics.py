075367912acf886f60f9e9102def533cb600835b
K.-Michael Aye <aye@ess.ucla.edu>
2014-02-09 17:35:20 -0800

    adding planck function from MODIS calib document. Most likely it is different for a spatial integration that was included, because the difference in the pre-factor seems to be PI

--- physics.py
+++ physics.py
@@ -3,31 +3,55 @@ from __future__ import division
 import matplotlib
 matplotlib.use('Qt4Agg')
 from scipy.constants import k as kb
-from scipy.constants import c,h
+from scipy.constants import c, h
 import numpy as np
 from matplotlib.pyplot import figure, show
- 
+
+
+def exp_term(T, wavelength=None, nu=None):
+    if nu == None:
+        return np.exp( (h*c) / (wavelength*kb*T) )
+    else:
+        return np.exp( (h*nu) / (kb*T) )
+
+
 def planck_wavelength(T, wavelength):
     nom = 2*h*c*c
-    denom = wavelength**5*(np.exp( h*c/(kb*T*wavelength)) - 1)
+    denom = wavelength**5 * (exp_term(T, wavelength=wavelength) - 1)
     return nom/denom
-    
+
+
+def planck_frequency(T, nu):
+    nom = 2*h*nu*nu*nu
+    denom = c * c * (exp_term(T, nu=nu) - 1)
+    return nom/denom
+
+
+def planck_modis(T, wave):
+    c1 = 3.741e-16
+    c2 = 1.4393e-2
+    denom = wave**5 * (np.exp( (c2/(wave*T))) -1 )
+    return c1/denom
+
+
 def rayleigh_jeans(T, wavelength):
     nom = 2*kb*c*T
     denom = wavelength**4
     return nom/denom
-    
+
+
 def wien_approx(T, wavelength):
     nom = 2*h*c*c
     denom = wavelength**5
     eterm = -h*c/(wavelength*kb*T)
     return nom/denom * np.exp(eterm)
-    
+
+
 if __name__ == '__main__':
     temps = np.arange(0, 500)
     waves = np.linspace(300,3000, 100)
     waves = waves*1e-9
-    
+
     fig = figure()
     ax = fig.add_subplot(111)
     for t in range(1000,7500,1000):
