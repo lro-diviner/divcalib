cb021fcbd823f9c50ab94153bec478b2758b6f17
K.-Michael Aye <aye@ess.ucla.edu>
2014-02-19 09:22:05 -0800

    wip

--- apps/plot_ground_calib.py
+++ apps/plot_ground_calib.py
@@ -3,13 +3,14 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from datetime import timedelta
 
 root = '/raid1/maye/ground_calib/'
 
-s = pd.read_hdf(root+'gc_b1_11.h5','df')
+df = pd.read_hdf(root+'gc_a6_b1_b2_b3.h5','df')
 
-print(len(s))
+print(df.info())
 
 fig, ax = plt.subplots(figsize=(80,10))
 
@@ -17,20 +18,23 @@ fmt = '%Y%m%d %H:%M:%S'
 deltahours = timedelta(hours=12)
 start = pd.Timestamp('20080110 21:00:00')
 
+def do_plot(df, ax, ymin, ymax):
+    df[df < ymin] = np.nan
+    df[df > ymax] = np.nan
+    df.plot(rasterized=True, ax=ax)
+
 while True:
     end = start + deltahours
     print(start,end)
-    fig, ax = plt.subplots(figsize=(120, 3))
-    roi = s[start.strftime(fmt):end.strftime(fmt)]
+    fig, axes = plt.subplots(nrows=2, figsize=(120, 4))
+    roi = df[start.strftime(fmt):end.strftime(fmt)]
     if len(roi) == 0:
         break
     roi = roi.resample('1s')
-    roi.plot(rasterized=True, ax=ax)
-    ymax = roi.mean()*1.1
-    ymin = roi.mean()*0.9
-    ax.set_ylim([ymin, ymax])
+    do_plot(roi['a6_11'], axes[0], 17000, 32000)
+    do_plot(roi['b1_11 b2_11 b3_11'.split()], axes[1], 21000, 32000)
     fmt2 = '%Y%m%d%H'
-    fname = 'b1_11_'+start.strftime(fmt2)+'-' + end.strftime(fmt2) + '.png'
+    fname = 'gc_a6_b1_b2_b3_d11_'+start.strftime(fmt2)+'-' + end.strftime(fmt2) + '.png'
     plt.savefig(fname, dpi=200)
     print("Created",fname)
     start = start + deltahours
