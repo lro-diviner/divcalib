c35025d962cea241338cfe5d9fbba5322a57524f
K.-Michael Aye <aye@ess.ucla.edu>
2014-03-12 12:07:18 -0700

    debugging unused format string. was not really a bug, but an unused format string

--- file_utils.py
+++ file_utils.py
@@ -331,8 +331,7 @@ def parse_times(df):
     format = '%d-%b-%Y %H:%M:%S.%f'
     # I don't need to round the seconds here because the df.utc data has 
     # already a 3-digit millisecond string: '19:00:00.793'
-    times = pd.to_datetime(df.date + ' ' + df.utc, format='%d-%b-%Y %H:%M:%S.%f',
-                           utc=False)
+    times = pd.to_datetime(df.date + ' ' + df.utc, format=format, utc=False)
     df.set_index(times, inplace=True)
     return df.drop(['date','utc'], axis=1)
 
