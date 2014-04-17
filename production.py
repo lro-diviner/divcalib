5365a2c41b2e4ec49b7a523095eb5aa221396b78
K.-Michael Aye <aye@ess.ucla.edu>
2014-04-17 14:50:28 -0700

    adding functions for an only_calibrate run through all data

--- production.py
+++ production.py
@@ -25,3 +25,32 @@ def get_example_data():
     rdr2.calibrate()
     rdr1 = rdrx.RDRR(tstr)
     return rdr1,rdr2
+def calibrate_fname(tstr, savedir):
+    print(tstr)
+    sys.stdout.flush()
+    df = fu.open_and_accumulate(tstr=tstr)
+    try:
+        if len(df) == 0:
+            return
+    except TypeError:
+        return
+    rdr2 = calib.Calibrator(df, fix_noise=True)
+    rdr2.calibrate()
+    rdr2.Tb.to_hdf(get_tb_savename(savedir, tstr), 'df')
+    rdr2.abs_radiance.to_hdf(get_rad_savename(savedir, tstr), 'df')
+
+
+def only_calibrate():
+    logging.basicConfig(filename='divcalib_only_calibrate.log',
+                        format='%(asctime)s %(message)s',
+                        level=logging.INFO)
+    
+    with open('/u/paige/maye/src/diviner/data/2010120102_2010122712.txt') as f:
+        timestrings = f.readlines()
+    
+    savedir = '/raid1/maye/rdr_out/only_calibrate'
+    Parallel(n_jobs=8, 
+             verbose=5)(delayed(calibrate_fname)(tstr.strip(),
+                                                        savedir)
+                                                        for tstr in timestrings)
+                                                        
