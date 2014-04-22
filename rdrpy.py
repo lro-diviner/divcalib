98772ec6d023d0379ed46fbfd28adc74d1ddf73d
K.-Michael Aye <aye@ess.ucla.edu>
2014-04-21 20:39:38 -0700

    pep8ified fixed get_example_data in rdrx.py

--- rdrpy.py
+++ rdrpy.py
@@ -11,52 +11,56 @@ from multiprocessing import Pool
 # the data itself is provided by the rdrp tool from Mark
 srcdir = '/raid1/marks/feidata/DIV:instTestRdr/data'
 
-# only save in 
+# only save in
 destdir = '/luna1/maye'
 
 # sourcefiles from where the timestamps of interest are determined
-fpaths = glob.glob(srcdir+'/*.zip')
+fpaths = glob.glob(srcdir + '/*.zip')
 
 # descriptor files that have not been converted to hdf yet
-despaths = glob.glob(destdir+'/*.des')
+despaths = glob.glob(destdir + '/*.des')
+
 
 def rdrp2hdf(fpath):
     """Again, fpath/fname only used to define the time-stamp for rdrp."""
     fname = os.path.basename(fpath)
     timestamp = fname.split('_')[0]
-    if os.path.exists(os.path.join(destdir,timestamp+'.h5')):
-        print timestamp,'exists.'
+    if os.path.exists(os.path.join(destdir, timestamp + '.h5')):
+        print timestamp, 'exists.'
         return
-    newfname = os.path.join(destdir,timestamp+'.des')
+    newfname = os.path.join(destdir, timestamp + '.des')
     cmd = 'rdrp daterange=' + timestamp + ' > ' + newfname
     print(cmd)
     call(cmd, shell=True)
     print("Produced {}.des".format(timestamp))
     des2hdf(newfname)
 
+
 def rdrp2des_or_csv(start, end=None, outtype=None):
     """start and end in format YYYYMM[DD[HH]]."""
-    if end == None:
+    if end is None:
         daterange_option = str(start)
     else:
-        daterange_option = "{0},{1}".format(start,end)
-    if outtype == None:
+        daterange_option = "{0},{1}".format(start, end)
+    if outtype is None:
         outtype = 'pipes'
         ext = '.des'
     else:
         ext = '.csv'
     outpath = os.path.join(destdir, str(start) + ext)
-    cmd = 'rdrp daterange={0} outtype={1} > {2}'.format(daterange_option,outtype,outpath)
+    cmd = 'rdrp daterange={0} outtype={1} > {2}'.format(
+        daterange_option, outtype, outpath)
     print(cmd)
     call(cmd, shell=True)
     print("Done.")
+
 
 def produce_h5():
     pool = Pool(3)
     if despaths:
         print "Found des files. First converting them:", despaths
         pool.map(des2hdf, despaths)
-    pool.map(rdrp2hdf, fpaths)    
+    pool.map(rdrp2hdf, fpaths)
 
 if __name__ == '__main__':
     outtype = None
@@ -69,4 +73,3 @@ if __name__ == '__main__':
             print("Usage: {0} START END [OUTTYPE]".format(sys.argv[0]))
             sys.exit()
     rdrp2des_or_csv(start, end, outtype)
-    
--- rdrx.py
+++ rdrx.py
@@ -2,21 +2,42 @@ import ana_utils as au
 from diviner import file_utils as fu
 import pandas as pd
 
+
+list_to_melt = [
+    'cemis',
+    'radiance',
+    'csunazi',
+    'clat',
+    'cloctime',
+    'clon',
+    'qge',
+    'qmi',
+    'qca',
+    'csunzen',
+    'counts',
+    'tb',
+    'vlookz',
+    'vlooky',
+    'vlookx'
+]
+
+
 def get_example_rdrx():
     tstr = '2013031707'
-    return RDRR(tstr)
+    obs = fu.DivObs(tstr)
+    return RDRR(obs.rdrrfname.path)
 
 
 def colnames(colbase, channel=None):
     "create rdrx column names for one or all channels."
     if not channel:
-        channels = range(1,10)
+        channels = range(1, 10)
     else:
         channels = [channel]
     colnames = []
     for c in channels:
-        colnames.extend([colbase+'_'+str(c)+'_'+str(i).zfill(2) 
-                            for i in range(1,22)])
+        colnames.extend([colbase + '_' + str(c) + '_' + str(i).zfill(2)
+                         for i in range(1, 22)])
     return colnames
 
 
@@ -24,22 +45,24 @@ def colnames(colbase, channel=None):
 #     cdetstr = '_'.join(colname.split('_')[1:])
 #     cdet = au.CDet(cdetstr)
 #     return cdet.
-    
+
 class RDRR(object):
-    """The RDRR object has 2948 columns. This class is a data extractor for it."""
+
+    """Class to enable extracting data from RDRx."""
+
     def __init__(self, tstr_or_filename):
         self.df = fu.RDRR_Reader(tstr_or_filename).open()
-        
+
     @property
     def columns(self):
         return self.df.columns
 
     def get_column(self, colbase):
-        return self.df.filter(regex='^'+colbase+'_')
-        
+        return self.df.filter(regex='^' + colbase + '_')
+
     def get_counts(self):
         return self.get_column('counts')
-        
+
     def get_molten_col(self, colbase, channel):
         rdr1 = self.df
         df = rdr1[colnames(colbase, channel)]
