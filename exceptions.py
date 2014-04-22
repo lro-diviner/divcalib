06de6499e7d9c518ec19ddf989d5e09de0f3d0c4
K.-Michael Aye <aye@ess.ucla.edu>
2014-04-21 20:27:03 -0700

    pep8ified

--- exceptions.py
+++ exceptions.py
@@ -12,11 +12,12 @@ class ViewLengthError(DivCalibError):
         self.view = view
         self.expected = expected
         self.received = received
+
     def __str__(self):
         return "Length of {0}-view not the expected {1}. "\
-                "Instead: {2}".format(self.view,
-                                      self.expected,
-                                      self.received)
+            "Instead: {2}".format(self.view,
+                                  self.expected,
+                                  self.received)
 
 
 class NoOfViewsError(DivCalibError):
@@ -25,16 +26,18 @@ class NoOfViewsError(DivCalibError):
         self.wanted = wanted
         self.value = value
         self.where = where
+
     def __str__(self):
         return "Number of {0} views not as expected in {3}. "\
-                "Wanted {1}, got {2}.".format(self.view, self.wanted,
-                                              self.value, self.where)
+            "Wanted {1}, got {2}.".format(self.view, self.wanted,
+                                          self.value, self.where)
 
 
 class UnknownMethodError(DivCalibError):
     def __init__(self, method, location):
         self.method = method
         self.location = location
+
     def __str__(self):
         return "Method {0} not defined here. ({1})".format(self.method,
                                                            self.location)
@@ -44,15 +47,17 @@ class WrongTypeError(DivCalibError):
     def __init__(self, type_required, type_current):
         self.type_required = type_required
         self.type_current = type_current
+
     def __str__(self):
         return "Wrong type {0} for requested operation. "\
-                "Need {1}".format(self.type_current,
-                                  self.type_required)
+            "Need {1}".format(self.type_current,
+                              self.type_required)
 
 
 class MeanTimeCalcError(DivCalibError):
     def __init__(self, t):
         self.t = t
+
     def __str__(self):
         return "Problem calculating mean time at hour {}".format(self.t)
 
@@ -61,17 +66,20 @@ class DivTimeLengthError(Exception):
     def __init__(self, tstr, expected):
         self.tstr = tstr
         self.expected = expected
+
     def __str__(self):
         return "Length of tstr {0} does not fit expected format {1}.".format(
-                                                self.tstr,
-                                                self.expected)
+            self.tstr,
+            self.expected)
 
 
-class RDRR_NotFoundError(Exception):
+class RDRR_NotFoundError(IOError):
     pass
 
-class RDRS_NotFoundError(Exception):
+
+class RDRS_NotFoundError(IOError):
     pass
 
-class L1ANotFoundError(Exception):
+
+class L1ANotFoundError(IOError):
     pass
