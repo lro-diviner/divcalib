430dc154cec88d1008c742a3c1591785d4818339
K.-Michael Aye <aye@ess.ucla.edu>
2014-04-29 22:04:48 -0700

    adding add_time_columns and commenting out advanced write_out funtions

--- production.py
+++ production.py
@@ -145,6 +145,18 @@ def grep_channel_and_melt(indf, colname, channel, obs):
     return c_molten
 
 
+def add_time_columns(df):
+    df.set_index('index', inplace=True)
+    df['hour'] = df.index.hour
+    df['minute'] = df.index.minute
+    df['second'] = df.index.second
+    df['micro'] = df.index.microsecond / 1000
+    df['year'] = df.index.year
+    df['month'] = df.index.month
+    df['date'] = df.index.day
+    df['second'] = df.second.astype('str') + '.' + df.micro.astype('str')
+
+
 def merge_rdr1_rdr2(tstr, c, savedir):
     channel = au.Channel(c)
     obs = fu.DivObs(tstr)
@@ -162,86 +174,87 @@ def merge_rdr1_rdr2(tstr, c, savedir):
     rdr2 = rdr1_merged.merge(tb_molten_c, left_on=mergecols,
                              right_on=mergecols)
     rdr2 = rdr2.merge(rad_molten_c, left_on=mergecols, right_on=mergecols)
+    add_time_columns(rdr2)
     return rdr2
 
-
-def prepare_rdr2_write(df):
-    pass
-
-
-def write_rdr2(df, formatter, fname):
-    formatter = Formatter()
-
-    # read in old RDR
-    print("Reading old RDR file.")
-    rdr = fu.RDRReader('/u/paige/maye/rdr_data/' + timestr + '_RDR.TAB.zip')
-    df = rdr.read_df(do_parse_times=False)
-    print("Done reading.")
-
-    # add cphase and roi
-    df['cphase'] = 123.45678
-    df['roi'] = 1234
-
-    # adapt to new format
-    # drop the old quality flags
-    df = df.drop(['qca', 'qge', 'qmi'], axis=1)
-
-    # flags = ['o', 'v', 'i', 'm', 'q', 'p', 'e', 'z', 't', 'h', 'd', 'n',
-    #          's', 'a', 'b']
-
-    set_orientation(df)
-    set_view(df)
-    set_instrument(df)
-    set_moving(df)
-    set_quality(df)
-    set_pointing(df)
-    set_ephemeris(df)
-    process_qmi(df)
-    set_noise(df)
-
-    for flag in flags:
-        df[flag] = 0
-
-    # filter for the channel requested:
-    df_ch = df[df.c == int(ch)]
-
-    # fill the nan values of your tb and radiance calculations with -9999.0
-    df_ch.fillna(-9999.0, inplace=True)
-
-    # create channel id:
-    chid = 'C' + ch
-
-    # open file and start write-loop
-    f = open(os.path.join(fu.outpath, timestr + '_' + chid + '_RDR.TAB'), 'w')
-
-    # header defined above, globally for this file.
-    print("Starting the write-out.")
-    f.write(header + '\r\n')
-    for i, data in enumerate(df_ch.values):
-        if all(data[22:30] < -5000):
-            fmt = formatter.nan
-        # spaceview
-        elif (all(data[22:24] > -5000)) and (all(data[24:30] < -5000)):
-            fmt = formatter.space
-        # solartarget view
-        elif (all(data[22:24] > -5000) and (all(data[24:27] < -5000))
-                and (all(data[27:29] > -5000))):
-            fmt = formatter.solartarget
-        # nominal
-        elif all(data[22:30] > -5000):
-            fmt = formatter.nominal
-        # uncaught case, raise exception to notify user!
-        else:
-            print(i)  # which line
-            print(data)  # print whole row
-            # point out af status, might help to understand
-            print("af:", data[14])
-            for j, item in enumerate(data[22:30]):
-                print(j + 22, item)
-            raise Exception
-        f.write(', '.join(fmt).format(*data) + '\r\n')
-    f.close()
 
 
 if __name__ == '__main__':
     only_calibrate()
+
+# def prepare_rdr2_write(df):
+#     pass
+
+
+# def write_rdr2(df, formatter, fname):
+#     formatter = Formatter()
+
+#     # read in old RDR
+#     print("Reading old RDR file.")
+#     rdr = fu.RDRReader('/u/paige/maye/rdr_data/' + timestr + '_RDR.TAB.zip')
+#     df = rdr.read_df(do_parse_times=False)
+#     print("Done reading.")
+
+#     # add cphase and roi
+#     df['cphase'] = 123.45678
+#     df['roi'] = 1234
+
+#     # adapt to new format
+#     # drop the old quality flags
+#     df = df.drop(['qca', 'qge', 'qmi'], axis=1)
+
+#     # flags = ['o', 'v', 'i', 'm', 'q', 'p', 'e', 'z', 't', 'h', 'd', 'n',
+#     #          's', 'a', 'b']
+
+#     set_orientation(df)
+#     set_view(df)
+#     set_instrument(df)
+#     set_moving(df)
+#     set_quality(df)
+#     set_pointing(df)
+#     set_ephemeris(df)
+#     process_qmi(df)
+#     set_noise(df)
+
+#     for flag in flags:
+#         df[flag] = 0
+
+#     # filter for the channel requested:
+#     df_ch = df[df.c == int(ch)]
+
+#     # fill the nan values of your tb and radiance calculations with -9999.0
+#     df_ch.fillna(-9999.0, inplace=True)
+
+#     # create channel id:
+#     chid = 'C' + ch
+
+#     # open file and start write-loop
+#     f = open(os.path.join(fu.outpath, timestr + '_' + chid + '_RDR.TAB'), 'w')
+
+#     # header defined above, globally for this file.
+#     print("Starting the write-out.")
+#     f.write(header + '\r\n')
+#     for i, data in enumerate(df_ch.values):
+#         if all(data[22:30] < -5000):
+#             fmt = formatter.nan
+#         # spaceview
+#         elif (all(data[22:24] > -5000)) and (all(data[24:30] < -5000)):
+#             fmt = formatter.space
+#         # solartarget view
+#         elif (all(data[22:24] > -5000) and (all(data[24:27] < -5000))
+#                 and (all(data[27:29] > -5000))):
+#             fmt = formatter.solartarget
+#         # nominal
+#         elif all(data[22:30] > -5000):
+#             fmt = formatter.nominal
+#         # uncaught case, raise exception to notify user!
+#         else:
+#             print(i)  # which line
+#             print(data)  # print whole row
+#             # point out af status, might help to understand
+#             print("af:", data[14])
+#             for j, item in enumerate(data[22:30]):
+#                 print(j + 22, item)
+#             raise Exception
+#         f.write(', '.join(fmt).format(*data) + '\r\n')
+#     f.close()
