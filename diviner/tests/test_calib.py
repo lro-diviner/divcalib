8bce838b1a142fd923c29ab829f56a98de5e84ce
K.-Michael Aye <kmichael.aye@gmail.com>
2014-06-30 18:39:01 -0600

    adding test for my noise correction implementation from Josh

--- diviner/tests/test_calib.py
+++ diviner/tests/test_calib.py
@@ -2,6 +2,11 @@ from __future__ import division, print_function
 import pytest
 from diviner import file_utils as fu
 from diviner import calib
+import socket
+from diviner.div_l1a_fix import correct_noise
+import numpy as np
+
+hostname = socket.gethostname().split('.')[0]
 
 
 @pytest.fixture
@@ -26,3 +31,28 @@ class TestCalBlock:
         df = fu.open_and_accumulate(tstr)
         rdr2 = calib.Calibrator(df)
         rdr2.calibrate()
+
+
+def test_noise_correction():
+    """Comparing my implementation of Josh's algorithm.
+
+    Josh made available some corrected files. I will simply compare
+    the outcome of his correction with output from mine.
+    """
+    # get original data
+    tstr = '2012011401'
+    obs = fu.DivObs(tstr)
+    data_in = obs.get_l1a()
+    # get Josh's corrected data
+    if hostname.startswith('luna'):
+        corr_data_dir = '/raid1/maye'
+    else:
+        corr_data_dir = '/Users/maye/data/diviner'
+    josh_corr = corr_data_dir + '/2012011401_L1A.TAB.corrected'
+    josh_corr = fu.read_l1a_data(josh_corr)
+    # apply my implementation
+    my_corr = correct_noise(data_in)
+    results = []
+    for det in calib.thermal_detectors:
+        results.append(np.allclose(my_corr[det], josh_corr[det]))
+    assert np.array(results).all()
