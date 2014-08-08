210aafc06c3d74ab38b7c3ea92a943e25587fec2
K.-Michael Aye <kmichael.aye@gmail.com>
2014-08-08 12:28:55 -0700

    pep8 and removing matplotlib QtAgg call

--- diviner/physics.py
+++ diviner/physics.py
@@ -1,7 +1,5 @@
 #!/Library/Frameworks/EPD64.framework/Versions/7.3/bin/enpkg
 from __future__ import division
-import matplotlib
-matplotlib.use('Qt4Agg')
 from scipy.constants import k as kb
 from scipy.constants import c, h
 import numpy as np
@@ -9,10 +7,10 @@ from matplotlib.pyplot import figure, show
 
 
 def exp_term(T, wavelength=None, nu=None):
-    if nu == None:
-        return np.exp( (h*c) / (wavelength*kb*T) )
+    if nu is None:
+        return np.exp((h*c) / (wavelength*kb*T))
     else:
-        return np.exp( (h*nu) / (kb*T) )
+        return np.exp((h*nu) / (kb*T))
 
 
 def planck_wavelength(T, wavelength):
@@ -30,7 +28,7 @@ def planck_frequency(T, nu):
 def planck_modis(T, wave):
     c1 = 3.741e-16
     c2 = 1.4393e-2
-    denom = wave**5 * (np.exp( (c2/(wave*T))) -1 )
+    denom = wave**5 * (np.exp((c2/(wave*T))) -1)
     return c1/denom
 
 
@@ -49,19 +47,20 @@ def wien_approx(T, wavelength):
 
 if __name__ == '__main__':
     temps = np.arange(0, 500)
-    waves = np.linspace(300,3000, 100)
+    waves = np.linspace(300, 3000, 100)
     waves = waves*1e-9
 
     fig = figure()
     ax = fig.add_subplot(111)
-    for t in range(1000,7500,1000):
-        planck = planck_wavelength(t,waves)
+    for t in range(1000, 7500, 1000):
+        planck = planck_wavelength(t, waves)
         # rj = rayleigh_jeans(t,waves)
         # wa = wien_approx(t,waves)
         # p_rj = planck/rj
         # p_wa = planck/wa
         # div by 1e6 to have radiance in per micron
-        ax.plot(waves*1e6, planck, label='{0}K, Radiance: {1:.1e}'.format(t,np.sum(planck/1e6)))
+        ax.plot(waves*1e6, planck, label='{0}K, Radiance: {1:.1e}'
+                .format(t, np.sum(planck/1e6)))
     ax.grid()
     ax.legend(loc='best')
     ax.set_xlabel('Wavelength [micron]')
