5d7498d6fd8986d8edcd133759fbc0fc8076bb6d
K.-Michael Aye <kmichael.aye@gmail.com>
2015-08-05 12:09:08 -0600

    PEP8-ing

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import sys
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 from setuptools.command.test import test as TestCommand
 
-pandas_version = '0.14.1'
+pandas_version = '0.16.1'
+
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -11,33 +13,33 @@ class PyTest(TestCommand):
         self.test_suite = True
 
     def run_tests(self):
-        #import here, cause outside the eggs aren't loaded
+        # import here, cause outside the eggs aren't loaded
         import pytest
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
 
 setup(
-    name = "Diviner",
-    version = "2.4.0b3",
-    packages = find_packages(),
+    name="Diviner",
+    version="2.4.0b3",
+    packages=find_packages(),
 
-    install_requires = ['pandas>='+pandas_version],
-    tests_require = ['pytest'],
+    install_requires=['pandas>=' + pandas_version],
+    tests_require=['pytest'],
 
-    cmdclass = {'test': PyTest},
+    cmdclass={'test': PyTest},
 
     entry_points={
         "console_scripts": [
             'scp_l1a = diviner.file_utils:scp_l1a_file'
-            ]
+        ]
     },
 
-    #metadata
-    author = "K.-Michael Aye",
-    author_email = "kmichael.aye@gmail.com",
-    description = "Software for the calibration of LRO Diviner data",
-    license = "BSD 2-clause",
-    keywords = "Diviner LRO calibration RDR",
-    url = "http:www.diviner.ucla.edu",
+    # metadata
+    author="K.-Michael Aye",
+    author_email="kmichael.aye@gmail.com",
+    description="Software for the calibration of LRO Diviner data",
+    license="BSD 2-clause",
+    keywords="Diviner LRO calibration RDR",
+    url="http:www.diviner.ucla.edu",
 )
