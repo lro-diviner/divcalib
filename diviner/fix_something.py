b8310a54c072e26d18973c56fb3f254e7527bef6
K.-Michael Aye <kmichael.aye@gmail.com>
2018-08-22 16:41:42 -0600

    fix print statements

--- diviner/fix_something.py
+++ diviner/fix_something.py
@@ -5,16 +5,16 @@ from noise import fix_columns
 
 fnames = glob.glob('/luna1/maye/*.h5')
 
+
 def fixing_columns(fname):
-    print 'Doing',fname
+    print('Doing', fname)
     store = pandas.HDFStore(fname)
     df = store[store.keys()[0]]
     fix_columns(df)
-    store[store.keys()[0]]=df
+    store[store.keys()[0]] = df
     store.close()
+
 
 p = Pool(4)
 
-p.map(fixing_columns,fnames)
-
- 
+p.map(fixing_columns, fnames)
