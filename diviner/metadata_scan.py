b1e4e93520e88b1e536325bd55ea493fa2616a06
K.-Michael Aye <kmichael.aye@gmail.com>
2018-08-22 16:43:57 -0600

    fix print statements

--- diviner/metadata_scan.py
+++ diviner/metadata_scan.py
@@ -11,20 +11,19 @@ fnames = glob.glob(root+"/20????.h5")
 fnames.sort()
 colname = 'last_el_cmd'
 
-for i,fname in enumerate(fnames):
-    if i % 4 ==0:
+for i, fname in enumerate(fnames):
+    if i % 4 == 0:
         gc.collect()
-        print gc.collect()
+        print(gc.collect())
     print fname
     df = pd.read_hdf(fname, 'df', columns=[colname])
     df['mybool'] = df[colname] == 90
     df['label'] = label(df.mybool)[0]
     df['time'] = df.index
     g = df.groupby('label')['time']
-    pd.DataFrame({'start':g.first(),
-                  'duration':g.last() - g.first()})[1:].to_hdf('./test_scans.h5',
-                                                           'df',
-                                                           mode='a',
-                                                           format='table',
-                                                           append=True)
-
+    pd.DataFrame({'start': g.first(),
+                  'duration': g.last() - g.first()})[1:].to_hdf('./test_scans.h5',
+                                                                'df',
+                                                                mode='a',
+                                                                format='table',
+                                                                append=True)
