b1e87450fbbc04bd0411d69b0012e80a6790c27f
K.-Michael Aye <kmichael.aye@gmail.com>
2019-02-09 14:45:57 -0700

    don't assign lambdas and blackened

--- diviner/production.py
+++ diviner/production.py
@@ -17,29 +17,33 @@ from diviner.exceptions import RDRS_NotFoundError
 from .bintools import cols_to_descriptor
 import configparser
 
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - '
-                              '%(message)s')
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - " "%(message)s")
 
-module_logger = logging.getLogger(name='diviner.production')
+module_logger = logging.getLogger(name="diviner.production")
 
-formats = pd.read_csv(path.join(diviner.__path__[0],
-                                'data',
-                                'joined_format_file.csv'))
+formats = pd.read_csv(path.join(diviner.__path__[0], "data", "joined_format_file.csv"))
 
 cols_no_melt = [i for i in formats.colname if i in rdrx.no_melt]
-cols_skip = 'c det tb radiance'.split()
+cols_skip = "c det tb radiance".split()
 cols_to_melt = set(formats.colname) - set(cols_no_melt) - set(cols_skip)
 cols_to_melt = [i for i in cols_to_melt if i in rdrx.to_melt]
 
 
 class Configurator(object):
-
-    def __init__(self, startstop=None, overwrite=False, c_start=3, c_end=9,
-                 do_rad_corr=False, swap_clons=True, save_as_pipes=True):
+    def __init__(
+        self,
+        startstop=None,
+        overwrite=False,
+        c_start=3,
+        c_end=9,
+        do_rad_corr=False,
+        swap_clons=True,
+        save_as_pipes=True,
+    ):
         # time scope of run definition
         self.start, self.stop = startstop
         self.tstrings = fu.calc_daterange(self.start, self.stop)
-        self.run_name = start + '_' + stop
+        self.run_name = start + "_" + stop
 
         self.overwrite = overwrite
 
@@ -50,18 +54,18 @@ class Configurator(object):
         self.swap_clons = swap_clons
         self.save_as_pipes = save_as_pipes
         if save_as_pipes is True:
-            self.out_format = 'bin'
+            self.out_format = "bin"
         else:
-            self.out_format = 'csv'
+            self.out_format = "csv"
 
         # set up paths
         self.savedir = self.paths.savedir
         self.rdr2_root = self.paths.rdr2_root
 
         # logging setup
-        logger = logging.getLogger(name='diviner')
+        logger = logging.getLogger(name="diviner")
         logger.setLevel(logging.DEBUG)
-        logfname = 'divcalib_verif_' + self.run_name + '.log'
+        logfname = "divcalib_verif_" + self.run_name + ".log"
         fh = logging.FileHandler(logfname)
         fh.setLevel(logging.DEBUG)
         ch = logging.StreamHandler(stream=None)
@@ -79,24 +83,23 @@ class Configurator(object):
     def get_rdr2_savename(self, tstr, c, savedir=None):
         if savedir is None:
             savedir = self.rdr2savedir
-        return path.join(savedir,
-                         '{0}_C{1}_RDR_2.{2}'.format(tstr, c, self.out_format))
+        return path.join(savedir, "{0}_C{1}_RDR_2.{2}".format(tstr, c, self.out_format))
 
 
 def get_tb_savename(savedir, tstr):
-    return path.join(savedir, tstr + '_tb.hdf')
+    return path.join(savedir, tstr + "_tb.hdf")
 
 
 def get_rad_savename(savedir, tstr):
-    return path.join(savedir, tstr + '_radiance.hdf')
+    return path.join(savedir, tstr + "_radiance.hdf")
 
 
 def get_rdr2_savename(savedir, tstr, c):
-    return path.join(savedir, '{0}_C{1}_RDR_2.CSV'.format(tstr, c))
+    return path.join(savedir, "{0}_C{1}_RDR_2.CSV".format(tstr, c))
 
 
 def get_example_data():
-    tstr = '2013031707'
+    tstr = "2013031707"
     df = fu.get_clean_l1a(tstr)
     rdr2 = calib.Calibrator(df)
     rdr2.calibrate()
@@ -105,7 +108,7 @@ def get_example_data():
 
 
 def calibrate_tstr(tstr, savedir, do_rad_corr):
-    module_logger.info('Calibrating {}'.format(tstr))
+    module_logger.info("Calibrating {}".format(tstr))
     sys.stdout.flush()
     df = fu.open_and_accumulate(tstr=tstr)
     try:
@@ -115,16 +118,16 @@ def calibrate_tstr(tstr, savedir, do_rad_corr):
         return
     rdr2 = calib.Calibrator(df, fix_noise=True, do_rad_corr=do_rad_corr)
     rdr2.calibrate()
-    rdr2.Tb.to_hdf(get_tb_savename(savedir, tstr), 'df')
-    rdr2.abs_radiance.to_hdf(get_rad_savename(savedir, tstr), 'df')
+    rdr2.Tb.to_hdf(get_tb_savename(savedir, tstr), "df")
+    rdr2.abs_radiance.to_hdf(get_rad_savename(savedir, tstr), "df")
 
 
 def only_calibrate(timestrings):
 
-    savedir = '/raid1/maye/rdr_out/only_calibrate'
-    Parallel(n_jobs=4,
-             verbose=5)(delayed(calibrate_tstr)
-                        (tstr.strip(), savedir) for tstr in timestrings)
+    savedir = "/raid1/maye/rdr_out/only_calibrate"
+    Parallel(n_jobs=4, verbose=5)(
+        delayed(calibrate_tstr)(tstr.strip(), savedir) for tstr in timestrings
+    )
 
 
 def melt_and_merge_rdr1(rdrxobject, c):
@@ -143,58 +146,55 @@ def melt_and_merge_rdr1(rdrxobject, c):
         container.append(rdrxobject.get_molten_col(col, c))
 
     # now merge each entry of the container, until it's empty (IndexError)
-    mergecols = 'index det'.split()
+    mergecols = "index det".split()
     res = None
     while True:
         try:
             if res is None:
                 res = container.pop()
-            res = res.merge(container.pop(),
-                            left_on=mergecols,
-                            right_on=mergecols)
+            res = res.merge(container.pop(), left_on=mergecols, right_on=mergecols)
         except IndexError:
             break
-    final = pd.merge(no_melts.reset_index(),
-                     res,
-                     left_on='index',
-                     right_on='index')
+    final = pd.merge(no_melts.reset_index(), res, left_on="index", right_on="index")
     return final
 
 
 def grep_channel_and_melt(indf, colname, channel, obs, invert_dets=True):
-    c = indf.filter(regex=channel.mcs+'_')[obs.time.tindex]
-    renamer = lambda x: int(x[-2:])
+    c = indf.filter(regex=channel.mcs + "_")[obs.time.tindex]
     # for telescope B (channel.div > 6):
     if (channel.div > 6) and invert_dets:
-        renamer = lambda x: 22 - int(x[-2:])
-    c = c.rename(columns=renamer)
-    c_molten = pd.melt(c.reset_index(), id_vars=['index'], var_name='det',
-                       value_name=colname)
+        c = c.rename(columns=lambda x: 22 - int(x[-2:]))
+    else:
+        c = c.rename(columns=lambda x: int(x[-2:]))
+    c_molten = pd.melt(
+        c.reset_index(), id_vars=["index"], var_name="det", value_name=colname
+    )
     return c_molten
 
 
 def add_time_columns(df):
-    tindex = pd.DatetimeIndex(df['index'])
-    df['hour'] = tindex.hour
-    df['minute'] = tindex.minute
-    df['second'] = tindex.second
-    df['micro'] = tindex.microsecond // 1000
-    df['year'] = tindex.year
-    df['month'] = tindex.month
-    df['date'] = tindex.day
-    df['second'] = df.second.astype('str') + '.' + df.micro.astype('str')
-    df.drop('micro', axis=1, inplace=True)
+    tindex = pd.DatetimeIndex(df["index"])
+    df["hour"] = tindex.hour
+    df["minute"] = tindex.minute
+    df["second"] = tindex.second
+    df["micro"] = tindex.microsecond // 1000
+    df["year"] = tindex.year
+    df["month"] = tindex.month
+    df["date"] = tindex.day
+    df["second"] = df.second.astype("str") + "." + df.micro.astype("str")
+    df.drop("micro", axis=1, inplace=True)
 
 
 def check_for_existing_files(config, tstr):
     channels_to_do = []
     # check which channels are not done yet, if so.
     all_done = True
-    for c in range(config.c_start, config.c_end+1):
+    for c in range(config.c_start, config.c_end + 1):
         fname = config.get_rdr2_savename(tstr, c)
         if path.exists(fname) and (not config.overwrite):
-            module_logger.debug("Found existing RDR2, skipping: {}"
-                                .format(path.basename(fname)))
+            module_logger.debug(
+                "Found existing RDR2, skipping: {}".format(path.basename(fname))
+            )
         else:
             all_done = False
             channels_to_do.append(c)
@@ -203,30 +203,28 @@ def check_for_existing_files(config, tstr):
 
 def get_data_for_merge(tstr, savedir):
     # start processing
-    module_logger.info('Processing {0}'.format(tstr))
+    module_logger.info("Processing {0}".format(tstr))
     obs = fu.DivObs(tstr)
     try:
         rdr1 = rdrx.RDRS(obs.rdrsfname.path)
     except RDRS_NotFoundError:
-        module_logger.warning('RDRS not found for {}'.format(tstr))
+        module_logger.warning("RDRS not found for {}".format(tstr))
         return
     if not path.exists(get_tb_savename(savedir, tstr)):
         try:
             calibrate_tstr(tstr, savedir, config.do_rad_corr)
         except:
-            module_logger.error('Calibration failed for {}'.format(tstr))
+            module_logger.error("Calibration failed for {}".format(tstr))
             return
     try:
-        tb = pd.read_hdf(get_tb_savename(savedir, tstr), 'df')
+        tb = pd.read_hdf(get_tb_savename(savedir, tstr), "df")
     except KeyError:
-        module_logger.error("Could not find key 'df' in tb file for {}"
-                            .format(tstr))
+        module_logger.error("Could not find key 'df' in tb file for {}".format(tstr))
         return
     try:
-        rad = pd.read_hdf(get_rad_savename(savedir, tstr), 'df')
+        rad = pd.read_hdf(get_rad_savename(savedir, tstr), "df")
     except KeyError:
-        module_logger.error("Could not find key 'df' in rad file for {}"
-                            .format(tstr))
+        module_logger.error("Could not find key 'df' in rad file for {}".format(tstr))
         return
     return obs, rdr1, tb, rad
 
@@ -243,8 +241,9 @@ def produce_tstr(args):
     all_done, channels_to_do = check_for_existing_files(config, tstr)
 
     if all_done:
-        module_logger.info("Not overwriting existing files for {}. Returning.".
-                           format(tstr))
+        module_logger.info(
+            "Not overwriting existing files for {}. Returning.".format(tstr)
+        )
         return
 
     try:
@@ -252,38 +251,36 @@ def produce_tstr(args):
     except Exception as e:
         module_logger.error("Error in get_data_for_merge: {}".format(e))
 
-    mergecols = ['index', 'det']
+    mergecols = ["index", "det"]
     for c in channels_to_do:
-        module_logger.debug('Processing channel {} of {}'.format(c, tstr))
+        module_logger.debug("Processing channel {} of {}".format(c, tstr))
         if not path.exists(rdr2savedir):
             os.mkdir(rdr2savedir)
         fname = config.get_rdr2_savename(tstr, c)
         channel = au.Channel(c)
         rdr1_merged = melt_and_merge_rdr1(rdr1, channel.div)
 
-        tb_molten_c = grep_channel_and_melt(tb, 'tb', channel, obs)
-        rad_molten_c = grep_channel_and_melt(rad, 'radiance', channel, obs)
+        tb_molten_c = grep_channel_and_melt(tb, "tb", channel, obs)
+        rad_molten_c = grep_channel_and_melt(rad, "radiance", channel, obs)
 
-        rdr2 = rdr1_merged.merge(tb_molten_c, left_on=mergecols,
-                                 right_on=mergecols)
+        rdr2 = rdr1_merged.merge(tb_molten_c, left_on=mergecols, right_on=mergecols)
         rdr2 = rdr2.merge(rad_molten_c, left_on=mergecols, right_on=mergecols)
         add_time_columns(rdr2)
         rdr2.fillna(-9999, inplace=True)
-        rdr2.det = rdr2.det.astype('int')
-        rdr2.drop('index', inplace=True, axis=1)
-        rdr2['c'] = channel.div
+        rdr2.det = rdr2.det.astype("int")
+        rdr2.drop("index", inplace=True, axis=1)
+        rdr2["c"] = channel.div
         clon_cols = rdr2.filter(regex="^clon_").columns
         if config.swap_clons:
             for col in clon_cols:
-                rdr2[col] = rdr2[col].map(lambda x: -(360 - x)
-                                          if x > 180 else x)
+                rdr2[col] = rdr2[col].map(lambda x: -(360 - x) if x > 180 else x)
         if not config.save_as_pipes:
             rdr2.to_csv(fname, index=False)
         else:
             # first put a descriptor file next to it if it doesn't exist
-            descpath = rdr2savedir + '/rdr2_verif.des'
+            descpath = rdr2savedir + "/rdr2_verif.des"
             if not os.path.exists(descpath):
-                with open(descpath, 'w') as f:
+                with open(descpath, "w") as f:
                     f.write(cols_to_descriptor(rdr2.columns))
             # now write the values in binary form
             rdr2.values.astype(np.double).tofile(fname)
@@ -291,7 +288,7 @@ def produce_tstr(args):
     return "{} done.".format(tstr)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     try:
         configfile = sys.argv[1]
@@ -303,30 +300,33 @@ if __name__ == '__main__':
     config.read(configfile)
 
     # paths and scope of production run
-    start = config.get('run_control', 'start')
-    stop = config.get('run_control', 'stop')
-    rad_tb_path = config.get('paths', 'rad_tb')
-    pipes_path = config.get('paths', 'pipes')
+    start = config.get("run_control", "start")
+    stop = config.get("run_control", "stop")
+    rad_tb_path = config.get("paths", "rad_tb")
+    pipes_path = config.get("paths", "pipes")
 
     # calibration control options
-    overwrite = config.getbool('calibration', 'overwrite')
-    c_start = config.getint('calibration', 'c_start')
-    c_end = config.getint('calibration', 'c_end')
-    do_rad_corr = config.getbool('calibration', 'do_rad_corr')
-    swap_clons = config.getbool('calibration', 'swap_clons')
-    save_as_pipes = config.getbool('calibration', 'save_as_pipes')
+    overwrite = config.getbool("calibration", "overwrite")
+    c_start = config.getint("calibration", "c_start")
+    c_end = config.getint("calibration", "c_end")
+    do_rad_corr = config.getbool("calibration", "do_rad_corr")
+    swap_clons = config.getbool("calibration", "swap_clons")
+    save_as_pipes = config.getbool("calibration", "save_as_pipes")
 
-    config = Configurator(startstop=(start, stop),
-                          overwrite=overwrite,
-                          do_rad_corr=do_rad_corr,
-                          save_as_pipes=save_as_pipes,
-                          swap_clons=swap_clons)
+    config = Configurator(
+        startstop=(start, stop),
+        overwrite=overwrite,
+        do_rad_corr=do_rad_corr,
+        save_as_pipes=save_as_pipes,
+        swap_clons=swap_clons,
+    )
     if not path.exists(config.rdr2savedir):
         os.mkdir(config.rdr2savedir)
 
     tstrings = config.tstrings
-    Parallel(n_jobs=8, verbose=20)(delayed(produce_tstr)((tstr, config))
-                                   for tstr in tstrings)
+    Parallel(n_jobs=8, verbose=20)(
+        delayed(produce_tstr)((tstr, config)) for tstr in tstrings
+    )
 
 
 # def prepare_rdr2_write(df):
