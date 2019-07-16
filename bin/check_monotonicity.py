e3da040d1d87962d1eff7a1ffb6a7048723f7736
K.-Michael Aye <aye@ess.ucla.edu>
2019-07-16 14:01:19 -0700

    black format

--- bin/check_monotonicity.py
+++ bin/check_monotonicity.py
@@ -1,30 +1,26 @@
 #!/usr/bin/env python
-from __future__ import print_function, division
 from diviner import file_utils as fu
 import logging
 from joblib import Parallel, delayed
-from diviner import divtweet
 import sys
 
-logging.basicConfig(filename='log_monotonicity.log', level=logging.INFO)
+logging.basicConfig(filename="log_monotonicity.log", level=logging.INFO)
+
 
 def check_fname(fname):
     df = fu.open_and_accumulate(fname)
     if not df.index.is_monotonic:
         logging.warning("{} has non-monotonic index".format(fname))
 
+
 def main():
-    found = False
-    try:
-        pump = fu.L1ADataPump(sys.argv[1])
-    except:
-        print("Use: {} year (4-digits)".format(sys.argv[0]))
-        sys.exit()
+    print("Use: {} year (4-digits)".format(sys.argv[0]))
+    pump = fu.L1ADataPump(sys.argv[1])
     todo = pump.fnames
 
     Parallel(n_jobs=8)(delayed(check_fname)(fname) for fname in todo)
-    divtweet.tweet_machine('now really: {} checked for monotonicity.'.format(sys.argv[1]))
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
 
