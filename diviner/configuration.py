ff53bb8ef60c4db43734b7415468eb2ed172fc22
K.-Michael Aye <aye@ess.ucla.edu>
2020-01-09 06:51:32 -0800

    add rdr2savedir and automatic creation

--- diviner/configuration.py
+++ diviner/configuration.py
@@ -9,7 +9,7 @@ from . import file_utils as fu
 
 config_fname = "diviner_production_config.toml"
 with resource_path("diviner.data", config_fname) as p:
-    default_config_path = p
+    default_config_path = Path(p)
 
 
 def read_config(fpath):
@@ -20,10 +20,40 @@ def copy_standard_config():
     "Copies the provided default config file into user's home."
     copy(default_config_path, Path.home())
 
+class Config:
+    def __init__(self):
+        # search in home
+        p = Path.home() / config_fname
+        if p.exists():
+            print(f"Found config {config_fname} in home directory.")
+            self.data = read_config(p)
+        else:
+            print("No config file found.")
+            return None
+
+    @property
+    def root(self):
+        return Path(self.data['paths']['root'])
+
+    @property
+    def rdr2savedir(self):
+        p = self.root / self.data['paths']['prodrun']
+        if not p.exists():
+            print(f"Creating {p}")
+            p.mkdir(parents=True)
+        return p
+
+    @property
+    def out_format(self):
+        return self.data['calibration']['out_format']
+
+    def get_rdr2_savename(self, tstr, c, savedir=None):
+        if savedir is None:
+            savedir = self.rdr2savedir
+        return path.join(savedir, "{0}_C{1}_RDR_2.{2}".format(tstr, c, self.out_format))
+
 
 class Configurator:
-    fname = "production_config.toml"
-    fpath = Path.home() / f".{fname}"
 
     savedir = "/luna4/maye/rdr_out/no_jpl_correction"
     # if not os.path.exists(savedir):
@@ -31,7 +61,13 @@ class Configurator:
     rdr2_root = "/luna4/maye/rdr_out/verification_no_jpl_corr"
     # if not os.path.exists(rdr2_root):
     #     os.o(rdr2_root)
+    @classmethod
+    def from_config_file(cls, fpath):
+        config = read_config(fpath)
+        tstart = config['run_control']['start']
+        tstop = config['run_control']['stop']
 
+        return cls()
     def __init__(
         self,
         tstart,
